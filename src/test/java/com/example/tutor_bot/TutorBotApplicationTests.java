fbcaad35240112b32ddd9083eb8011bc16072014
suyeon <93810963+syjungsuyeon@users.noreply.github.com>
2025-07-30 10:53:00 +0900

    init

---
+++ src/test/java/com/example/tutor_bot/TutorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutor_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
