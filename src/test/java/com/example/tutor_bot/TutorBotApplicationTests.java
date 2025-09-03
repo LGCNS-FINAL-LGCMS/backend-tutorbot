3157264f6272493cd8a47dfedb2d795b533a96bb
이재원 <ghfkddl706@me.com>
2025-09-03 14:08:33 +0900

    [LB-172] CI/CD 및 프로메테우스 액추에이터 설정

--- src/test/java/com/example/tutor_bot/TutorBotApplicationTests.java
+++ src/test/java/com/example/tutor_bot/TutorBotApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.tutor_bot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TutorBotApplicationTests {
 
